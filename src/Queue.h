3ecc8d82b70b9a1356232c28de57b54fc7c575a8
veidtz <miguel97381@gmail.com>
2026-04-29 20:28:50 -0600

    added files

---
+++ src/Queue.h
@@ -0,0 +1,29 @@
+#ifndef COLA_H
+#define COLA_H
+
+#include <iostream>
+using namespace std;
+
+const int SIZEC = 50;
+
+template <class T> class Queue {
+    private:
+        int front;
+        int tail;
+        int numElem;
+        T arrQueue[SIZEC];
+
+    public:
+        Queue();
+
+        int next(int n);
+        bool isEmpty();
+        bool isFull();
+        bool Insert(T element);
+        T Dequeue();
+        T Front();
+        int Size();
+        void Print();
+};
+
+#endif
